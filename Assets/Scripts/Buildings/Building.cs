111b322a6242d7d4874af1a89c287f9a27b17fb9
Thabb <49516328+Thabb@users.noreply.github.com>
2021-11-25 13:38:20 +0100

    ADD: Building
    Closes #3

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buildings
+{
+    /// <summary>
+    /// The base class for every kind of building. Shouldn't be instantiated on its own.
+    /// </summary>
+    public abstract class Building : MonoBehaviour
+    {
+        /// <summary>
+        /// A list of ResourcePackets that are used for the Production. This will need to be instantiated by the Start method of every child with the specific resources of that building.
+        /// </summary>
+        protected List<ResourcePacket> Input { get; set; }
+        /// <summary>
+        /// A list of ResourcePackets that are the product of the Production. This will need to be instantiated by the Start method of every child with the specific resources of that building.
+        /// </summary>
+        protected List<ResourcePacket> Output { get; set; }
+
+        /// <summary>
+        /// Starts the Production Coroutine.
+        /// </summary>
+        protected void Start()
+        {
+            StartCoroutine(Production());
+        }
+
+        /// <summary>
+        /// This method takes resources from the input and places some resources in the output instead. This will be different for every child.
+        /// </summary>
+        /// <returns>IEnumerator: Production of a building should always run and not hinder the game by it.</returns>
+        protected abstract IEnumerator Production();
+    }
+}
