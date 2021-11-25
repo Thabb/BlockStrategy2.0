e8a4e09fe0c4a857a23d86c3715bb0f0bf5a6ca3
Thabb <49516328+Thabb@users.noreply.github.com>
2021-11-25 14:02:03 +0100

    ADD: Container
    Closes #5

---
+++ Assets/Scripts/Buildings/Container.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Buildings
+{
+    /// <summary>
+    /// A small utility class that is used for testing purposes to store items without the need for a building.
+    /// </summary>
+    public class Container : Building
+    {
+        /// <summary>
+        /// This method is not needed in this special case of a "building".
+        /// </summary>
+        /// <returns>IEnumerator: - </returns>
+        protected override IEnumerator Production()
+        {
+            yield return null;
+        }
+    }
+}
